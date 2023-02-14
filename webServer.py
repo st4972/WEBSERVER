e2c91a179c6ca5eccfd75e3dfec7f59f725b0c34
Smit Kashyap Trivedi <112817199+st4972@users.noreply.github.com>
2023-02-14 16:07:24 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -31,25 +31,25 @@ def webServer(port=13331):
             outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
             # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
             # Content-Type above is an example on how to send a header as bytes
+            connectionSocket.send(outputdata.encode())
             # Fill in end
 
             # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
-            connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
             # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
             # Fill in start
-
+            connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
             # Fill in end
 
             # Send the content of the requested file to the client
             for i in f:  # for line in file
             # Fill in start - send your html file contents #Fill in end
-              connectionSocket.send(f"{f[i].encode()}\r\n")
+              connectionSocket.send(f"{f[i].encode()}\r\n\r\n")
             connectionSocket.close()  # closing the connection socket
 
         except Exception as e:
     # Send response message for invalid request due to the file not being found (404)
     # Fill in start
-          connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+          connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
     # Fill in end
 
     # Close client socket
